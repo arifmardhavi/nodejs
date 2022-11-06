04560eea89e3b6a08b372bdafb6a2beab3a26cd7
M. Arif Mardhavi <arifmardhavi@gmail.com>
2022-11-06 15:48:58 +0700

    commit

---
+++ node-final-project-v1/getter.js
@@ -0,0 +1,17 @@
+export function mapUsers(users) {
+  return {
+    data : users,
+    count : users.length
+  }
+  // return result; // TODO: replace this
+}
+
+export function mapArticles(articles) {
+  return {
+    data : articles,
+    count : articles.length
+  }
+  // return result; // TODO: replace this
+}
+
+// TODO: answer here
---
+++ node-final-project-v1/main.js
@@ -0,0 +1,53 @@
+// TODO: answer here
+import * as dotenv from "dotenv";
+import {mapUsers,mapArticles} from "./getter.js";
+dotenv.config()
+
+function mergeData(usersData, articlesData) {
+  const data = mapUsers(usersData)
+  const result = {
+    projectName : process.env.PROJECT_NAME,
+    accessToken : process.env.ACCESS_TOKEN,
+    users : mapUsers(usersData),
+    articles : mapArticles(articlesData)
+  }
+  return result; // TODO: replace this
+}
+
+const users = [
+  {
+    id: 1,
+    username: "isrotrip",
+    password: "PhT1Oky01",
+    gender: "male",
+  },
+  {
+    id: 2,
+    username: "dmtrxw",
+    password: "oYt2Thj9w",
+    gender: "male",
+  }
+]
+
+const articles = [
+  {
+    id: 1,
+    article: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    userId: 1,
+  },
+  {
+    id: 2,
+    article: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    userId: 5,
+  },
+  {
+    id: 3,
+    article: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    userId: 4,
+  },
+]
+
+console.log(mergeData(users,articles));
+// console.log(mapUsers(users));
+// console.log(mapArticles(articles));
+// TODO: answer here
